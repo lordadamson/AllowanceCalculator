b753cc348c643474fea71bd0f9ec6ba6f89b4a59
Adam Zahran <lordadamson@gmail.com>
2015-04-07 00:50:10 +0300

    just a little comment

--- app/src/main/java/com/example/adam/pockethourglass/MainActivity.java
+++ app/src/main/java/com/example/adam/pockethourglass/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//this is a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
